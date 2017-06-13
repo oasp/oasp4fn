2ce892da61312b16d59895de5dc844afdc888fe3
CORP\dalfonso <david.alfonso-garcia@capgemini.com>
2017-06-13 12:52:05 +0200

    .gitignore update

--- dist/adapters/fn-cognito.d.ts
+++
@@ -1,4 +0,0 @@
-import { FnAuthService } from '../index'
-
-declare let cognito: FnAuthService
-export default cognito
--- dist/adapters/fn-dynamo.d.ts
+++
@@ -1,5 +0,0 @@
-
-import { FnDBService } from '../index'
-
-declare let dynamo: FnDBService
-export default dynamo
