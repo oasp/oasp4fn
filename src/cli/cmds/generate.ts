17ff7f32a2c4df81c6c05bab7333cd61d16213ae
Dario Rodriguez <dario.rodriguez-gonzalez@capgemini.com>
2018-03-14 19:05:47 +0100

    Refactoring CLI. Now we are using yargs

---
+++ src/cli/cmds/generate.ts
@@ -0,0 +1,53 @@
+import { Arguments } from 'yargs';
+import { run } from '../../generators/index';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as _ from 'lodash';
+import * as chalk from 'chalk';
+
+export const command: string = 'generate [provider]';
+export const desc: string = 'Generate serverless.yml and webpack.js based on your project';
+
+export const builder = (yargs: Arguments) => {
+    yargs.usage('Usage: $0 generate [provider] [Options]')
+        .positional('provider', {
+            describe: 'Serverless provider',
+            choices: ['aws', 'azure', 'google', 'openwhisk'],
+            default: 'aws'
+        }).options({
+            'opts': {
+                alias: 'o',
+                type: 'string',
+                nargs: 1,
+                describe: 'file with the options for the yml generation' },
+            'path': {
+                alias: 'p',
+                type: 'string',
+                nargs: 1,
+                describe: 'directory where the handlers are stored'},
+            'express': {
+                alias: 'e',
+                type: 'boolean',
+                desc: 'generates an express app.ts file'}
+        }).example('$0 generate aws', 'Generate files for aws provider').version(false);
+};
+
+export const handler = function (argv: Arguments) {
+    let opts: any = {
+        config: argv.opts,
+        app: argv.e,
+        path: argv.path
+    };
+
+    if (opts.config) {
+        if (fs.existsSync(opts.config)) {
+            _.assign(opts, require(`${process.cwd()}${path.sep}${opts.config}`));
+            opts = _.omit(opts, 'config');
+        } else
+            console.log(`${chalk.red('Error!')}\nThe options file "${opts.config}" doesn't exist.`);
+    } else if (fs.existsSync('oasp4fn.config.js')) {
+        _.assign(opts, require(`${process.cwd()}${path.sep}oasp4fn.config.js`));
+    }
+
+    _.isEmpty(opts) ? run() : run(opts);
+};
